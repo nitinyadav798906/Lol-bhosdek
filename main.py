a557e91830edd04be35db481b6c41665c21d6d9d
nitinyadav798906 <nitinyadav798906@gmail.com>
2025-05-22 12:31:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ cookies_file_path = os.getenv("COOKIES_FILE_PATH", "youtube_cookies.txt")
 OWNER_ID = 1714266885 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [1714266885 1577263404]
+SUDO_USERS = [1714266885]
 
 AUTH_CHANNEL = -1002542634912
 
